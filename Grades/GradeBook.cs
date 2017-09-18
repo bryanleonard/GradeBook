3e5ab3dcc37d6fd6337ee924b1d7d7bcd9142b07
Bryan Leonard <bryan@forcequit.ip>
2017-09-17 22:11:49 -0400

    Initial commti.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        // methods are a member of this class
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        //a "field" for storing a list of floating point numbers
+        //be sure to initialize, otherwise you get null reference error
+        //List<float> grades = new List<float>();
+        // Don't need to write it like above since we made a GradeBook class w/grade declaration
+        private List<float> grades;
+
+    }
+} 
+
+//Class Members Define
+// 1. State (nouns)
+// 2. Behavior (verbs)
+
+// GradeBook State
+// 1. The grades for a user
+
+// GradeBook Behavior
+// 1. Add a new grade
+// 2. Calculate stats
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -10,6 +10,29 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f); //f allows it to convert to a float
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+
         }
     }
 }
+
+//https://app.pluralsight.com/player?course=c-sharp-fundamentals-with-visual-studio-2015&author=scott-allen&name=c-sharp-fundamentals-with-visual-studio-2015-m3&clip=4&mode=live
+
+//access modifiers - start there.
+// public - makes a class memember publicly available. 
+// private - (default) only usable from code insie of the same class
+
+// static - use static class members t/o creating an instance
+// for example: public static float MinGrade = 0;   // GradeBook.MinGrade;
+//              public static float MaxGrade = 100; // GradeBook.MaxGrade;
