fcf86bd0e5e042e00be310c09dbaf4c10eec7059
Bryan Leonard <bleonard@acrtinc.com>
2017-09-22 15:02:55 -0400

    Final commit, I thnk.

---
+++ Grades.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4, 4);
+            Assert.AreEqual(3, 4);
+        }
+    }
+}
